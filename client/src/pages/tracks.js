dd447acb5411b69bf38021b31796e59853f29bee
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-07-07 00:28:32 +0300

    feat: add response rendering

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { gql, useQuery } from '@apollo/client';
 import { Layout } from '../components';
+import TrackCard from '../containers/track-card';
+
 
 /**
  * Tracks Page is the Catstronauts home page.
@@ -25,12 +27,13 @@ export const TRACKS = gql`
 `;
 const Tracks = () => {
   const { loading, error, data } = useQuery(TRACKS);
-
   if (loading) return 'Loading...';
-
   if (error) return `Error! ${error.message}`;
-
-  return <Layout grid>{JSON.stringify(data)}</Layout>;
+  return <Layout grid>
+    {data?.tracksForHome?.map((track, index) => (
+      <TrackCard key={index} track={track} />
+    ))}
+  </Layout>;
 };
 
 export default Tracks;
