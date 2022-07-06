6d24e8ecc035865ec909b158d3e6b47db7b6d6ec
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-07-06 23:54:21 +0300

    feat: add useQuery hook

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import { gql, useQuery } from '@apollo/client';
 import { Layout } from '../components';
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
+
+/** TRACKS query to retrieve all tracks */
+export const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        name
+        photo
+      }
+    }
+  }
+`;
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  if (loading) return 'Loading...';
+
+  if (error) return `Error! ${error.message}`;
+
+  return <Layout grid>{JSON.stringify(data)}</Layout>;
 };
 
 export default Tracks;
