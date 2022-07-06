7ce814331e825a038b58c3429bae17b8cab24d90
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-07-07 00:38:13 +0300

    feat: add loading and error states

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { gql, useQuery } from '@apollo/client';
-import { Layout } from '../components';
+import { Layout, QueryResult } from '../components';
 import TrackCard from '../containers/track-card';
-
 
 /**
  * Tracks Page is the Catstronauts home page.
@@ -27,13 +26,16 @@ export const TRACKS = gql`
 `;
 const Tracks = () => {
   const { loading, error, data } = useQuery(TRACKS);
-  if (loading) return 'Loading...';
-  if (error) return `Error! ${error.message}`;
-  return <Layout grid>
-    {data?.tracksForHome?.map((track, index) => (
-      <TrackCard key={index} track={track} />
-    ))}
-  </Layout>;
+
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track, index) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
