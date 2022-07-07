df33f9c3e1d9f48d2597e76dbbbf6be302c204f3
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-07-07 14:35:23 +0300

    feat: add resolvers to ApolloServer

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,7 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+const TrackAPI = require('./datasources/track-api');
 
 
 const mocks = {
@@ -25,7 +27,12 @@ const mocks = {
 
 const server = new ApolloServer({
   typeDefs,
-  mocks
+  resolvers,
+  dataSources: () => {
+    return {
+      trackAPI: new TrackAPI()
+    };
+  }
 });
 
 
