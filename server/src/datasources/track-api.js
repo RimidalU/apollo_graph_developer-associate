e4b4f0399ac636da435e040aa73ab572acbbc2ee
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-07-08 14:42:17 +0300

    feat: add metod incrementTrackViews to the data source

--- server/src/datasources/track-api.js
+++ server/src/datasources/track-api.js
@@ -22,6 +22,11 @@ class TrackAPI extends RESTDataSource {
   getTrackModules(trackId) {
     return this.get(`track/${trackId}/modules`);
   }
+
+  incrementTrackViews(trackId) {
+    return this.patch(`track/${trackId}/numberOfViews`);
+  }
+
 }
 
 module.exports = TrackAPI;
