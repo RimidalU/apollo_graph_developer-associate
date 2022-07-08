88acad3de137a33f7b008cbfa8468cb33d44b201
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-07-08 13:37:58 +0300

    docs: copy the final state of the project

--- final/server/src/datasources/track-api.js
+++ final/server/src/datasources/track-api.js
@@ -14,6 +14,22 @@ class TrackAPI extends RESTDataSource {
   getAuthor(authorId) {
     return this.get(`author/${authorId}`);
   }
+
+  getTrack(trackId) {
+    return this.get(`track/${trackId}`);
+  }
+
+  getTrackModules(trackId) {
+    return this.get(`track/${trackId}/modules`);
+  }
+
+  getModule(moduleId) {
+    return this.get(`module/${moduleId}`);
+  }
+
+  incrementTrackViews(trackId) {
+    return this.patch(`track/${trackId}/numberOfViews`);
+  }
 }
 
 module.exports = TrackAPI;
