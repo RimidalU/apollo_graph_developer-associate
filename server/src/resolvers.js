13ddd0ceecc0c789e3b75c73add7f6572b66a9e6
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-07-08 16:45:20 +0300

    feat: add handling the error case

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -10,19 +10,32 @@ const resolvers = {
       return dataSources.trackAPI.getTrack(id);
     },
   },
+
   Mutation: {
     // increments a track's numberOfViews property
     incrementTrackViews: async (_, { id }, { dataSources }) => {
-      const track = await dataSources.trackAPI.incrementTrackViews(id);
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
 
-      return {
-        code: 200,
-        success: true,
-        message: `Successfully incremented number of views for track ${id}`,
-        track
-      };
-    },
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track
+        };
+
+      } catch (err) {
+        console.log(err.extensions);
+        return {
+          code: err.extensions.response.status,
+          success: false,
+          message: err.extensions.response.body,
+          track: null
+        };
+      }
+    }
   },
+
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
@@ -31,5 +44,6 @@ const resolvers = {
       return dataSources.trackAPI.getTrackModules(id);
     },
   }
-};
+}
+
 module.exports = resolvers;
