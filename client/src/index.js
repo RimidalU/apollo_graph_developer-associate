3bb7cb51e9e6624642c91999162c25a21757bc87
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-07-11 21:27:05 +0300

    refactor: refactor server url from heroku

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,8 @@ import GlobalStyles from './styles';
 import Pages from './pages';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  // uri: 'http://localhost:4000',
+  uri: "https://catstronaut-server-rirmi.herokuapp.com/", // change to YOUR own production server
   cache: new InMemoryCache(),
 });
 
